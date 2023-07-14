d1b9cfc995c62fd489aee500a33df92217b6e1a6
ktn-andrea <ktn.andrea@gmail.com>
2023-07-14 02:34:40 +0200

    Created ReportGenerator class

--- scripts/main.py
+++ scripts/main.py
@@ -7,13 +7,18 @@ from typing import List
 
 
 FILE_PATH = "scripts/data.json"
+POSSIBLE_TYPES = ["title", "text", "list", "picture", "plot"]
+POSSIBLE_KEYS = ["type", "title", "content", "configuration"]
 
 def main():
 
     raw_data = DataHandler(FILE_PATH)
     slides_data: List[dict] = raw_data.get_data_from_file()
 
-    print(slides_data)
+    #print(slides_data)
+
+    generator = ReportGenerator(POSSIBLE_TYPES, POSSIBLE_KEYS)
+    generator.generate_report(slides_data)
 
 
 
--- scripts/report_generator.py
+++ scripts/report_generator.py
@@ -2,14 +2,77 @@
 
 from pptx import Presentation
 from data_handler import DataHandler
+from typing import List
 
+SLIDE_LAYOUT_MODE = 0 
+'''
+0: Title (Title Slide)
+1: Text (Title and Content)
+2: List (Title and Content)
+3: Picture (Blank? , 6)
+4: Plot (Blank? , 6)
+'''
+
+""" Slide types: 
+0 ->  title and subtitle
+1 ->  title and content
+2 ->  section header
+3 ->  two content
+4 ->  Comparison
+5 ->  Title only 
+6 ->  Blank
+7 ->  Content with caption
+8 ->  Pic with caption
+"""
 
 class ReportGenerator():
-    def __init__(self):
-        Report = Presentation()
+    def __init__(self, possible_types: List, possible_keys: List):
+        report = Presentation()
+        self.possible_types = possible_types
+        self.possible_keys = possible_keys
 
 
-    def generate_slide():
+    def keys_validation(self, keys):
+        for key in keys:
+            if key not in self.possible_keys:
+                return False
+        return True
+
+    def generate_report(self, data: List[dict]):
+        for slide in data:
+            are_keys_valid = self.keys_validation(slide.keys())
+            if are_keys_valid == False:
+                print("Error: invalid key found")
+
+            else:
+                slide_type = slide["type"].lower()
+                #print(slide)
+                if slide_type not in self.possible_types:
+                    print("Error: slide type not supported")
+                else:
+                    if slide_type == "title":
+                        pass
+                    elif slide_type == "text":
+                        pass
+                    elif slide_type == "list":
+                        pass
+                    elif slide_type == "picture":
+                        pass
+                    elif slide_type == "plot":
+                        pass
+    
+
+    def generate_title_slide():
         pass
 
+    def generate_text_slide():
+        pass
 
+    def generate_list_slide():
+        pass
+
+    def generate_picture_slide():
+        pass
+
+    def generate_plot_slide():
+        pass
