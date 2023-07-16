84e331b526f1b8ca202ecf8a4a2d1c09625944c0
ktn-andrea <ktn.andrea@gmail.com>
2023-07-17 01:51:58 +0200

    Implemented logging

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3.10
 
-from pptx import Presentation
+import sys, logging, timeit, pathlib
+from typing import List 
 from report_generator import ReportGenerator
 from data_handler import DataHandler
-from typing import List 
-import logging, timeit
-import sys
 
 
-FILE_PATH = "data/data.json" #TODO get file_path as argument
 POSSIBLE_TYPES = ["title", "text", "list", "picture", "plot"]
 POSSIBLE_KEYS = ["type", "title", "content", "configuration"]
 
@@ -16,29 +13,39 @@ def main():
     '''
     Takes a valid .json file as an argument and uses it to generate a .pptx report.
     '''
+    FILE_PATH = pathlib.Path(sys.argv[1])
 
-    start = timeit.default_timer()
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-    if sys.version_info[0] != 3:
-        raise Exception("Python version 3 is needed to run this program.")
-    
-    logging.info('Program started')
-
-
-    raw_data = DataHandler(FILE_PATH)
+    raw_data: type[DataHandler] = DataHandler(FILE_PATH)
     slides_data: List[dict] = raw_data.get_data_from_file()
 
-    generator = ReportGenerator(POSSIBLE_TYPES, POSSIBLE_KEYS)
+    generator: type[ReportGenerator] = ReportGenerator(POSSIBLE_TYPES, POSSIBLE_KEYS)
     generator.generate_report(slides_data)
 
-
-    end = timeit.default_timer()
+    end: float = timeit.default_timer()
     logging.info('Finished in: {} seconds.'.format(end-start))
-
 
 
 ##################################################################
 
 if __name__ == '__main__':
-    main()
+
+    logging.basicConfig(filename='./data/log.txt', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    if sys.version_info[0] != 3:
+        logging.error("Python version 3 is needed to run this program.")
+    
+    if len(sys.argv) == 2:
+        arg_extension = pathlib.Path(sys.argv[1]).suffix
+        if arg_extension == ".json":
+            logging.info('Application started')
+            start: float = timeit.default_timer()
+            main()
+        else: 
+            logging.fatal("Please provide one input .json file as an argument.")
+            sys.exit()
+    else:
+        logging.fatal("Please provide one input .json file as an argument.")
+        sys.exit()
+    
+        
